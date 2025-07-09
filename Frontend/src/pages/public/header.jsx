995710bf0f13224c451cb0a16d8b2e351c70b091
sugreem-Nishad997 <sugreemn86@gmail.com>
2025-07-09 14:33:18 +0530

    header UI updated

--- Frontend/src/pages/public/header.jsx
+++ Frontend/src/pages/public/header.jsx
@@ -16,7 +16,7 @@ import IconButton from '@mui/material/IconButton';
 import "../../styles/header.css";
 import { Button } from '@mui/material';
 import { AuthContext } from '../../Context/AuthContext';
-import { Logout } from '@mui/icons-material';
+import { Close, FilterFramesSharp, FolderShared, Logout, Menu, PowerSettingsNewSharp, Person } from '@mui/icons-material';
 
 
 const CartBadge = styled(Badge)`
@@ -30,6 +30,7 @@ export default function header() {
     const [dropDown, setDropDown] = useState(false);
     const [input, setInput] = useState('');
     const [focus, setFocus] = useState(false);
+    const [open, setOpen] = useState(false);
     const { user, logout } = useContext(AuthContext);
 
     const handleform = (event) => {
@@ -49,36 +50,65 @@ export default function header() {
     return (
 
         <div className='headerContainer'>
-            <div class="offcanvas offcanvas-start bg-light text-black" tabindex="-1" id="offcanvasMenu" style={{ width: '60%' }}>
-                <div class="offcanvas-header" style={{ backgroundColor: 'rgb(35,111, 211)', color: 'white' }}>
-                    <AccountCircle sx={{ marginRight: '0.5rem' }} />
-                    <h5 class="offcanvas-title" onClick={() => {user?navigate("/account"):navigate("/auth")}} >{user?user.name:'Login & Signup'}</h5>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
+
+            <div className={`clientSidebar1 ${open ? 'open' : ''}`} style={{ padding: '0rem' }}>
+                <div className="close-btn">
+                    <Close onClick={() => setOpen(false)} sx={{ fontSize: 24, cursor: 'pointer' }} />
                 </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link text-black" onClick={()=>navigate("/account/orders")}>My Orders</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-black" onClick={() => navigate("/cart")}>My Cart</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-black" onClick={()=>navigate("/account/wishlist")}>My Wishlist</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-black" onClick={()=>navigate("/account")}>My Account</a>
-                        </li>
-                    </ul>
+
+                <div className="clientContainer1">
+                    <div className='profile' style={{ margin: '0rem' }}>
+                        <div style={{ margin: '0.9rem' }}>
+                            <Person />
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <p style={{ fontSize: '0.7rem', marginBottom: '0rem' }}>Hello,</p>
+                            <h6>{user ? user.isAdmin?user.name+'(Admin)' :user.name: 'User'}</h6>
+                        </div>
+                    </div>
+                    <div className='profileManagement'>
+                        <div onClick={() => navigate("/account/orders")}>
+                            <div className='icon-div order'>
+                                <FilterFramesSharp sx={{ color: 'rgb(41, 42, 43)' }} />
+                                <h6 className='icon-tag' >My Orders</h6>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='icon-div' onClick={()=>navigate("/account")}>
+                                <Person sx={{ color: 'rgb(41, 42, 43)' }} />
+                                <h6 className='icon-tag'>My Account</h6>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='icon-div' onClick={()=>navigate("/cart")}>
+                                <ShoppingCart sx={{ color: 'rgb(41, 42, 43)' }} />
+                                <h6 className='icon-tag'>MY Cart</h6>
+                            </div>
+                        </div>
+                        <div className='icon-div' onClick={()=>navigate("/account/wishlist")}>
+                            <Favorite sx={{ color: 'rgb(41, 42, 43)' }} />
+                            <h6 className='icon-tag'>MY Wishlist</h6>
+                        </div>
+                    </div>
+                    {user && <div style={{ position: 'relative', top: '19rem', backgroundColor: 'white' }}>
+                        <div className='icon-div'>
+                            <PowerSettingsNewSharp sx={{ color: 'rgb(5, 101, 190)' }} />
+                            <h6 className='icon-tag' onClick={() => {
+                                logout()
+                                navigate("/")
+                            }}>LOGOUT</h6>
+                        </div>
+                    </div>}
                 </div>
+
             </div>
             <div className='container'>
-                <nav class="navbar navbar-light bg-light" style={{ position: 'relative', top: '-0.8rem' }}>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" style={{ border: 'none', backgroundColor: 'white' }}>
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </nav>
-                <div className='logo' onClick={()=>navigate("/")}>
+                {!open && (
+                    <div className="mobile-toggle-btn" style={{ padding: '0rem', top: '0.35rem' }}>
+                        <Menu onClick={() => setOpen(true)} sx={{ fontSize: 30, cursor: 'pointer', color: 'black' }} />
+                    </div>
+                )}
+                <div className='logo' onClick={() => navigate("/")}>
                     <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/fkheaderlogo_exploreplus-44005d.svg" alt="" />
                 </div>
                 <div className='responsiveSearchA'>
@@ -111,10 +141,10 @@ export default function header() {
                 </div>
                 <div>
                     <div className="mobileLogin">
-                        <Button sx={{ color: 'black' }} startIcon={<AccountCircle />} onClick={() =>{ user?navigate("/account"):navigate("/auth")}}>{user?'YOU':'Login'}</Button>
+                        <Button sx={{ color: 'black' }} startIcon={<AccountCircle />} onClick={() => { user ? user.isAdmin ? navigate("/admin") : navigate("/account") : navigate("/auth") }}>{user ? user.isAdmin ? 'Admin' : 'YOU' : 'Login'}</Button>
                     </div>
                     <div className='login' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                        <Button sx={{ color: 'black', ":hover": { backgroundColor: 'rgb(44, 89, 212)', color: 'white' } }} startIcon={<AccountCircle />} onClick={() => { user ? navigate("/account") : navigate("/auth") }}>{user ? user.name : 'Login'} {dropDown ? <ArrowUp /> : <ArrowDown />}</Button>
+                        <Button sx={{ color: 'black', width: 'fitContent', ":hover": { backgroundColor: 'rgb(44, 89, 212)', color: 'white' } }} startIcon={<AccountCircle />} onClick={() => { user ? user.isAdmin ? navigate("/admin") : navigate("/account") : navigate("/auth") }} className='elipse'>{user ? user.isAdmin ? 'Mr. ADMIN' : user.name : 'Login'} {dropDown ? <ArrowUp /> : <ArrowDown />}</Button>
                     </div>
                     <ul className="dropDownList" style={{ display: dropDown === false ? 'none' : '', padding: '0.6rem' }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                         <li className='list-1' style={{ display: user ? 'none' : '' }}>
@@ -125,10 +155,10 @@ export default function header() {
                             <Button fullWidth sx={{ color: "black", ':hover': { color: 'white', backgroundColor: 'rgb(0, 102, 255)' }, justifyContent: 'flex-start' }} startIcon={<AccountCircle />} onClick={() => navigate("/account")}>My Profile</Button>
                         </li>
                         <li>
-                            <Button fullWidth sx={{ color: "black", ':hover': { color: 'white', backgroundColor: 'rgb(0, 102, 255)' }, justifyContent: 'flex-start' }} startIcon={<Order />}>Orders</Button>
+                            <Button fullWidth sx={{ color: "black", ':hover': { color: 'white', backgroundColor: 'rgb(0, 102, 255)' }, justifyContent: 'flex-start' }}onClick={()=>navigate("/account/orders")} startIcon={<Order />}>Orders</Button>
                         </li>
                         <li>
-                            <Button fullWidth sx={{ color: "black", ':hover': { color: 'white', backgroundColor: 'rgb(0, 102, 255)' }, justifyContent: 'flex-start' }} startIcon={<Favorite />}>Wishlist</Button>
+                            <Button fullWidth sx={{ color: "black", ':hover': { color: 'white', backgroundColor: 'rgb(0, 102, 255)' }, justifyContent: 'flex-start' }} onClick={()=>navigate("/account/wishlist")} startIcon={<Favorite />}>Wishlist</Button>
                         </li>
                         <li style={{ display: user ? '' : 'none' }}>
                             <Button fullWidth sx={{ color: "black", ':hover': { color: 'white', backgroundColor: 'rgb(0, 102, 255)' }, justifyContent: 'flex-start' }} startIcon={<Logout />} onClick={() => {
