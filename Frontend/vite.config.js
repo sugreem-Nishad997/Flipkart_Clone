aca787a03d08d898897879b5a8629e4d68f1abbb
sugreem-Nishad997 <sugreemn86@gmail.com>
2026-03-25 08:03:18 +0530

    Configure build options in vite.config.js
    optimize vite config for production

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    outDir: 'dist',
+
+    // Increase warning limit (optional)
+    chunkSizeWarningLimit: 700,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
